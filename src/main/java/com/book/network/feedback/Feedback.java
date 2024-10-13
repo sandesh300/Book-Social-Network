e208b40f82852ffb546d1166aff6ba7963939e9b
sandesh300 <bhujbalsandesh52@gmail.com>
2024-10-13 19:40:52 +0530

    add Feedback.java, to create the Feedback entity

---
+++ src/main/java/com/book/network/feedback/Feedback.java
@@ -0,0 +1,29 @@
+package com.book.network.feedback;
+
+import com.book.network.book.Book;
+import com.book.network.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    @Column
+    private Double note;
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+}
