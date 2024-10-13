38adbaed87bd4d4768a84707bc3e58558fe4df37
sandesh300 <bhujbalsandesh52@gmail.com>
2024-10-13 19:46:22 +0530

    add BookTransactionHistory.java, to create the Book transaction entity

---
+++ src/main/java/com/book/network/history/BookTransactionHistory.java
@@ -0,0 +1,31 @@
+package com.book.network.history;
+
+import com.book.network.book.Book;
+import com.book.network.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    // @ManyToOne
+    @Column(name = "user_id")
+    private String userId;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    private boolean returned;
+    private boolean returnApproved;
+}
